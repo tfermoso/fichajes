41ae6f82fb5c9dfaf6361f6c7f4573eb9f46142d
tfermoso <tomas.fermoso@gmail.com>
2025-03-25 20:04:08 +0100

    menús

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 import com.formacom.Fichajes;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner leer=new Scanner(System.in);
+        final String MENU_ADMINISTRADOR= """
+                1. Nuevo Alumno
+                2. Seleccionar Modo
+                3. Ver Informes
+                4. Salir
+                """;
+        final String MENU_ALUMNO= """
+                1. Fichar
+                2. Salir
+                """;
+        final String MENU_INFORME= """
+                1. Fichajes por dia
+                2. Fichajes por alumno
+                3. Volver
+                """;
 
     }
 }
